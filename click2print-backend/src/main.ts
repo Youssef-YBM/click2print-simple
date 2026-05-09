aa5f478088b1af2f4a4830e460a1f434aa9a7e50
noureddine <noureddineoubraim1@gmail.com>
2026-05-09 10:32:26 +0100

    version finale

--- click2print-backend/src/main.ts
+++ click2print-backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
 app.enableCors({ origin: ['http://localhost:5173', 'http://localhost:5174'] });
 
   await app.listen(3000);
-  console.log('✅ Backend Click2Print running on http://localhost:3000');
+  console.log('Backend Click2Print running on http://localhost:3000');
 }
 bootstrap();
